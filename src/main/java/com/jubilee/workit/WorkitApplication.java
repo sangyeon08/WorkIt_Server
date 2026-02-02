d65af5b3be00cc299add002619d45abc8355a221
slee <sanoni0115@gmail.com>
2026-02-03 10:54:42 +1100

    fix: linking DB server

--- src/main/java/com/jubilee/workit/WorkitApplication.java
+++ src/main/java/com/jubilee/workit/WorkitApplication.java
@@ -3,17 +3,13 @@ package com.jubilee.workit;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-
-import static java.awt.SystemColor.info;
 
 @Slf4j
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.jubilee.workit"})
 public class WorkitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorkitApplication.class, args);
 		log.info("\n\n============= started =============\n\n");
 	}
-
 }
